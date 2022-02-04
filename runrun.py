dd740bfdedf451ec87fa5fb089795242dc94cd36
Minsoo Lee <patrioticboy00@hotmail.com>
2022-02-05 02:11:57 +0900

    현재가 / 3프로 가

--- runrun.py
+++ runrun.py
@@ -3,8 +3,6 @@ import datetime
 import time
 import requests
 
-#Alarm 과 Autotrade 되도록
-
 access = "access"
 secret = "secret"
 myToken = "myToken"
@@ -45,20 +43,20 @@ while True:
         ma30 = get_ma30("KRW-BTC")
         eprice = end_price("KRW-BTC")
 
-        if now.minute == 1 or now.minute == 30:
+        if now.second == 1 or now.second == 30:
 
             if eprice[-1] < ma30[-1]:# and eprice[-1] >= ma30[-1]:
                 krw = get_balance("KRW")
-                if krw > 5000: #5000이하는 거래되지 않음
-                    buy_result = upbit.buy_market_order("KRW-BTC", krw*0.9995) #전재산의 0.9995 buy
+                if krw > 5000:
+                    buy_result = upbit.buy_market_order("KRW-BTC", krw*0.9995)
                     post_message(myToken, "#getrich", "Coin buy : " + str(buy_result))
-            elif eprice[-1] > ma30[-1]:# and eprice[-1] < ma30[-1]:
+            elif eprice[-1] > ma30[-1]:
                 btc = get_balance("BTC")
                 if btc > 0.00008:
                     sell_result = upbit.sell_market_order("KRW-BTC", btc*0.9995)
                     post_message(myToken, "#getrich", "Coin sell : " + str(sell_result))
 
-        time.sleep(57)
+        time.sleep(1)
 
     except Exception as e:
         post_message(myToken,"#getrich", e)
